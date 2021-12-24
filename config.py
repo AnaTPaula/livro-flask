9e42354583dba3b62a919be77f51bd9a94a0612e
Ana Terezinha <aterezinha.19@hotmail.com>
2021-12-24 12:24:02 -0300

    Alterar as configurações para acesso ao BD, alterações na classe MODEL (Category, Product, Role, User)

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 
+# CREATE DATABASE livro_flask CHARACTER SET UTF8 collate utf8_general_ci
+
 class Config:
     CSRF_ENABLE = True  # Habilita o uso de criptografia no flask
     SECRET = 'AOPYU76%#AS'
     TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     APP = None  # Receberá as propriedades do app
+
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://user:passwd@host:port/databae'
 
 
 class DevelopmentConfig(Config):
