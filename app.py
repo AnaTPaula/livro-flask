c8c36bc7c026fca847cbf0e8ac7b811eb353cb34
Ana Terezinha <aterezinha.19@hotmail.com>
2021-12-28 22:42:29 -0300

    Configurar rotas da aplicação

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 # config import
@@ -19,6 +19,36 @@ def create_app(config_name):
 
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return 'Meu primeiro run'
+
+    @app.route('/login/')
+    def login():
+        return 'Aqui entrará a tela de login'
+
+    @app.route('/recovery-password/')
+    def recovery_password():
+        return 'Aqui entrará a tela de recuperar senha'
+
+    @app.route('/profile/<int:user_id>/action/<action>/')
+    def profile(user_id, action):
+        if action == 'action1':
+            return f'Ação1 {action} usuário de ID {user_id}'
+        elif action == 'action2':
+            return f'Ação2 {action} usuário de ID {user_id}'
+        else:
+            return f'Ação default for userId {user_id}'
+
+    @app.route('/profile/', methods=['POST'])
+    def create_profile():
+        username = request.form['username']
+        password = request.form['password']
+        return f'Essa rota possui um método POST e criará um usuário com os dados de usuário {username} e senha ' \
+               f'{password}'
+
+    @app.route('/profile/<int:id>', methods=['PUT'])
+    def edit_total_profile(id):
+        username = request.form['username']
+        password = request.form['password']
+        return f'PUT username: {username}, password: {password}'
 
     return app
